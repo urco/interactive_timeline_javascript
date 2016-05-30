0f002b417ac381a8c27376d7f84a4a742e7e851d
urco <jareyva@hotmail.com>
2016-05-30 12:14:23 +0200

    implementing groups

--- client/main.js
+++ client/main.js
@@ -4,3 +4,102 @@ import { ReactiveVar } from 'meteor/reactive-var';
 import './main.html';
 
 
+
+// this variable will store the visualisation so we can delete it when we need to 
+
+Meteor.subscribe('Shows');
+
+  
+  Template.episodes.helpers ({
+    // returns an array of the names of all features of the requested type
+    
+    get_episodes : function(){
+      // pull an example song from the database
+      // - we'll use this to find the names of all the single features
+       var visjsobj;
+       var episodes_a = []; 
+       // create a data set with groups
+       var seasons = [];
+       var seassonsAux = 0 ;
+      
+      shows = Shows.findOne(); 
+
+      if (shows != undefined) {// looks good! 
+        // get an array of all the song feature names 
+        // (an array of strings)
+        console.log("hay datos");
+        var episodes = shows._embedded.episodes.length;
+        
+        
+       //create items dataset
+       var items = new vis.DataSet();
+
+         //collecting episodes/items 
+          
+        for (var i=0;i < episodes;i++){
+
+            episodes_a[i] = shows._embedded.episodes[i];
+            season = episodes_a[i].season;
+            seasons.push(season); 
+
+            items.add({
+                group:season,
+                content: 'Episode ' + episodes_a[i].number +
+          ' <span>' + episodes_a[i].name + '</span>',//episodes_a[i].name + '.' + '<span>Episode</span>' + episodes_a[i].number + '<span>,Season</span>' + episodes_a[i].season,
+                start:   episodes_a[i].start
+              });
+          }
+        
+     // new dataset for groups
+
+        var groups = new vis.DataSet();
+
+        var counterSeasons = Math.max.apply(Math, seasons); 
+        //console.log('numero de temporadas' + counterSeasons);
+  
+          for (var g = 1; g <= counterSeasons; g++) {
+            //var gAux = g +1;
+            groups.add({ //loading data group
+              id: g, 
+              value: g,
+              content:'<span>SEASON</span>' + g});
+          }
+          console.log(groups);
+          //console.log(groups);
+
+        //create VIS object. Timeline vis.js
+      
+        if (visjsobj != undefined){
+          visjsobj.destroy();
+        }
+        var options = {
+              /* groupOrder: function (a, b) {
+                  return b.id - a.id
+              }, */
+               type:'point',
+               showMajorLabels: false,
+               start:'2011',
+               end:'2017',
+               autoResize:true,
+               min:'2011'
+          };
+       
+        var container = document.getElementById('visualization');
+        //var timeline = new vis.Timeline(container, groups, items, options);
+        var timeline = new vis.Timeline(container);
+        timeline.setOptions(options);
+        timeline.setGroups(groups);
+        timeline.setItems(items);
+       
+        //timeline.fit();
+
+        return episodes_a; 
+
+      }
+      else {
+        return [];
+
+      }
+    }
+});
+   
--- server/server.js
+++ server/server.js
@@ -2,5 +2,95 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
-  
+ 
+
+
+  if (!Shows.findOne()) {
+
+		console.log("No hay episodios todavia... cargando datos");
+		// pull in the NPM package 'fs' which provides
+		// file system functions
+		var fs = Meteor.npmRequire('fs');
+		// get a list of files in the folder private/jsonfiles, which
+		// ends up as assets/app/jsonfiles in the app once it is built
+		var file = fs.readdirSync('./assets/app/jsonfiles/');
+		
+				
+		var filename = 'jsonfiles/game_of_thrones.json';
+		console.log(filename);
+	
+		var show = JSON.parse(Assets.getText(filename));
+		show = JSON.parse(JSON.stringify(show).split('"airdate":').join('"start":'));
+		var showFormated = JSON.stringify(show);
+		//show = JSON.parse(showFormated);
+		console.log(showFormated);
+		
+		Shows.insert(show);
+		console.log("JSON insertado correctamente");
+
+	}else {
+	
+		console.log('Hay datos');
+	}
+		 	// in case the file does not exist, put it in a try catch
+		 	/*try{
+		 		var song = JSON.parse(Assets.getText(filename));
+		 		// now flatten the rhythm and tonal features
+		 		// into a single set of properties
+		 		
+		 		var single_features = {};
+		 		var array_features = {};
+		 		var string_features = {};
+
+		 		rhythm_keys = Object.keys(song.rhythm);
+      			tonal_keys = Object.keys(song.tonal);
+      			for (var j=0;j<rhythm_keys.length;j++){
+      				console.log("type of "+rhythm_keys[j]+" is "+typeof(song.rhythm[rhythm_keys[j]]));
+      				// only use features that are numbers ... ignore arrays etc. 
+      				if (typeof(song.rhythm[rhythm_keys[j]]) === "number"){
+      					single_features[rhythm_keys[j]] = song.rhythm[rhythm_keys[j]];
+      				}
+      				if (typeof(song.rhythm[rhythm_keys[j]]) === "object" && 
+      					song.rhythm[rhythm_keys[j]].length != undefined){// its an array
+      					array_features[rhythm_keys[j]] = song.rhythm[rhythm_keys[j]];
+      				}
+      				if (typeof(song.rhythm[rhythm_keys[j]]) === "string"){
+      					string_features[rhythm_keys[j]] = song.rhythm[rhythm_keys[j]];
+      				}
+      	
+      			}
+      			for (var j=0;j<tonal_keys.length;j++){
+      				console.log("type of "+tonal_keys[j]+" is "+typeof(song.tonal[tonal_keys[j]]));
+      				if (typeof(song.tonal[tonal_keys[j]]) === "number"){
+      					single_features[tonal_keys[j]] = song.tonal[tonal_keys[j]];
+      				}
+      				if (typeof(song.tonal[tonal_keys[j]]) === "object" && 
+      					song.tonal[tonal_keys[j]].length != undefined){// its an array
+      					array_features[tonal_keys[j]] = song.tonal[tonal_keys[j]];
+      				}
+      				if (typeof(song.tonal[tonal_keys[j]]) === "string"){
+      					string_features[tonal_keys[j]] = song.tonal[tonal_keys[j]];
+      				}
+      			}
+		 		// insert the song to the DB:
+		 		// 
+		 		song.single_features = single_features;
+		 		song.array_features = array_features;
+		 		song.string_features = string_features;
+		 		
+		 		Songs.insert(song);
+		 		inserted_songs ++;
+		 	}catch (e){
+		 		console.log("error parsing file "+filename);
+		 	}
+		}
+		console.log("Inserted "+inserted_songs+" new songs...");*/
+
+
+
+	//publish methods
+
+   	Meteor.publish('Shows', function(){
+   	return Shows.find({})
+   });
 });
